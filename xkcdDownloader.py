cbd676570ec93fd5ad2d485ab3c41c201316d543
Matt Giguere <matthew.giguere@gmail.com>
2014-05-22 17:03:58 -0400

    working version
    It’s now fully functional

--- xkcdDownloader.py
+++ xkcdDownloader.py
@@ -7,19 +7,24 @@ Created on 2014-05-22T12:04:34
 from __future__ import division, print_function
 import sys
 import argparse
+import Image
+import cStringIO
+from PIL import ImageFont
+from PIL import ImageDraw
+import os
 
 try:
-    import numpy as np
+    from bs4 import BeautifulSoup
 except ImportError:
-    print('You need numpy installed')
+    print('You need BeautifulSoup installed')
     sys.exit(1)
 
 try:
-    import matplotlib.pyplot as plt
-    got_mpl = True
+    import urllib2
 except ImportError:
-    print('You need matplotlib installed to get a plot')
-    got_mpl = False
+    print('You need urllib2 installed')
+    sys.exit(1)
+
 
 __author__ = "Matt Giguere (github: @mattgiguere)"
 __maintainer__ = "Matt Giguere"
@@ -27,43 +32,113 @@ __email__ = "matthew.giguere@yale.edu"
 __status__ = " Development NOT(Prototype or Production)"
 __version__ = '0.0.1'
 
-params = {
-    #'backend': 'png',
-    'axes.linewidth': 1.5,
-    'axes.labelsize': 24,
-    'axes.font': 'sans-serif',
-    'axes.fontweight': 'bold',
-    'text.fontsize': 22,
-    'legend.fontsize': 14,
-    'xtick.labelsize': 16,
-    'ytick.labelsize': 16,
-    'text.usetex': False,
-    'font.family': 'Palatino'
-}
-plt.rcParams.update(params)
 
-
-def xkcdDownloader(arg1, arg2):
+def xkcdDownloader(imgdir, imtype):
     """PURPOSE: To download all XKCD comics (with alt-text) to the specified
         directory and in the specified format."""
+
+    if imgdir is None:
+        imgdir = 'xkcd_images/'
+    if imgdir[-1] != '/':
+        imgdir += '/'
+
+    if imtype is None:
+        imtype = 'png'
+
+    if not os.path.exists(imgdir):
+        os.makedirs(imgdir)
+
+    xkcdArchiveURL = 'http://xkcd.com/archive/'
+    xkcdArchiveObj = urllib2.urlopen(xkcdArchiveURL)
+    xkcdArchivePage = xkcdArchiveObj.read()
+
+    soup = BeautifulSoup(xkcdArchivePage)
+
+    nonComicLinksBefore = 7
+    nonComicLinksAfter = 13
+    links = soup.find_all('a')[nonComicLinksBefore:-nonComicLinksAfter]
+
+    hrefs = []
+    for link in links:
+        hrefs.append(link.get('href'))
+
+    for href in hrefs:
+        print("now working on comic: " + href[1:-1])
+        xkcdComicPageObj = urllib2.urlopen("http://xkcd.com"+href)
+        xkcdComicPage = xkcdComicPageObj.read()
+        soup = BeautifulSoup(xkcdComicPage)
+
+        #extract just the div with the comic:
+        comic = soup.find_all('div', attrs={"id": "comic"})
+
+        #ONLY continue from this point on if the comic has an
+        #image (and not some fancy javascript like comci 1350):
+        if comic[0].find('img') is not None:
+            #extract the comic image URL and load it as im.
+            comicUrl = comic[0].find('img')['src']
+            file = cStringIO.StringIO(urllib2.urlopen(comicUrl).read())
+            im = Image.open(file)
+
+            #get the size of the old image so we know how much padding to
+            #add for the alt-text:
+            oldImSize = im.size
+
+            #break up the alt-text string:
+            #each fixed-width character is 10 pixels. Figure out how
+            #many characters can fit per line based on the width of the
+            #comic image:
+            imTextLength = int(oldImSize[0] / 10.)
+
+            #save the comic alt-text to string:
+            altText = comic[0].find('img')['title']
+
+            #get the length of the alt-text string:
+            altTextLen = len(altText)
+
+            #now figure out how many lines you'll need below the image
+            #to fit all the alt-text:
+            numLines = int(altTextLen / imTextLength) + 1
+
+            #create a new list to contain the broken up al-text into
+            #each line at the bottom of the image:
+            newAltText = []
+            for i in range(numLines):
+                newAltText.append(altText[i * imTextLength:
+                                  (i + 1) * imTextLength])
+
+            #the heigh for each line of text:
+            textHeight = 25
+
+            #create the new Image with the correct amount of padding
+            #on the bottom
+            newIm = Image.new("RGB", [oldImSize[0],
+                              oldImSize[1] + textHeight * numLines], "white")
+
+            #now paste the image in with no offset from the top-left corner:
+            newIm.paste(im, (0, 0))
+            draw = ImageDraw.Draw(newIm)
+
+            #use a fixed-width (Mac-default) font to simplify things
+            font = ImageFont.truetype("/Library/Fonts/Andale Mono.ttf", 16)
+            for i in range(numLines):
+                draw.text((0, oldImSize[1] + i * textHeight),
+                          newAltText[i], (0, 0, 0), font=font)
+            newIm.save(imgdir+href[1:-1]+'.'+imtype, imtype)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='argparse object.')
     parser.add_argument(
-        'arg1',
-        help='This argument does something.')
+        'imgdir',
+        help='This the directory to download all the images to. ' +
+        'If not specified, all images will be downloaded into a directory ' +
+        'called "xkcd_images".', nargs='?')
     parser.add_argument(
-        'arg2',
-        help='This argument does something else. By specifying ' +
-             'the "nargs=>" makes this argument not required.',
+        'imtype',
+        help='This argument specifies the image type to save everything as. ',
              nargs='?')
-    if len(sys.argv) > 3:
-        print('use the command')
-        print('python filename.py tablenum columnnum')
-        sys.exit(2)
 
     args = parser.parse_args()
 
-    xkcdDownloader(int(args.arg1), args.arg2)
+    xkcdDownloader(args.imgdir, args.imtype)
